b6c7373a54fecbd85c48bdf448d74c2e94dda474
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-28 20:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+	function displayTime() {
+			let date = new Date();
+			let time = date.toLocaleTimeString();
+			let formattedDate = date.toLocaleDateString();
+			document.getElementById('timer').innerHTML = `${time}<br>${formattedDate}`;
+		}
+		setInterval(displayTime, 1000); // Update time every second
